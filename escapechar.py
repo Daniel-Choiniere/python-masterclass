4818b8223a20a81db38e110671c472ef9fa2e484
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-01-24 22:59:26 -0500

    escape characters, new lines, and using triple quotes to avoid using backslash to escape or taking new lines literally as it was coded

---
+++ escapechar.py
@@ -0,0 +1,19 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print("The pet shop owner said 'No, no, \'e\'s uh,...he\'s resting'.")
+# or you can do the same with this syntax
+print("The pet shop owner said \'No, no, 'e's uh,...he's resting\'.")
+# or using triple quotes you don't need an escape backslash
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting". """)
+
+# when using triple quotes it will take your newlines literally
+anotherSplitString = """This string
+has been 
+split
+over several lines"""
+
+print(anotherSplitString)
