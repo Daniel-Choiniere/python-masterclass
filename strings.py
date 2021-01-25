998eba6c8fe41c3504026096b615a933e2ebb785
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-01-24 22:42:09 -0500

    working with the Python print function

---
+++ strings.py
@@ -0,0 +1,11 @@
+print("Today is the day I start to learn Python")
+print("we can even include 'quotes' inside of strings")
+# string concatenation
+print("hello" + " world")
+
+# assigning to variables
+greeting = "Hello"
+name = input("Please enter your name ")
+
+# if we want a space we can add that also
+print(greeting + ' ' + name)
