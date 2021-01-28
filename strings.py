ca237589193b609ffa47a3ac00b98fe5619c1bda
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-01-28 10:12:42 -0500

    variable types, and rebounding variables

--- strings.py
+++ strings.py
@@ -5,7 +5,14 @@ print("hello" + " world")
 
 # assigning to variables
 greeting = "Hello"
-name = input("Please enter your name ")
-
+# name = input("Please enter your name ")
+name = "Dan"
+age = 35
 # if we want a space we can add that also
 print(greeting + ' ' + name)
+
+# checking the type of a variable
+
+print(type(greeting))
+print(type(age))
+
