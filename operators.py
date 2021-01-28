614f5615c3fa2046bd97306147d80fb4339caeb6
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-01-28 10:47:06 -0500

    mathematical operators, usinf integer divison to return whle number

---
+++ operators.py
@@ -0,0 +1,16 @@
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)  # integer division, rounded down towards minus infinity
+print(a % b)   # modulo: the remainder after integer division
+
+print()
+
+for i in range(1, 4):
+    print(i)
+
+# python is strongly typed and you cant use a float where an integer should be
--- strings.py
+++ strings.py
@@ -8,11 +8,11 @@ greeting = "Hello"
 # name = input("Please enter your name ")
 name = "Dan"
 age = 35
+
 # if we want a space we can add that also
 print(greeting + ' ' + name)
 
 # checking the type of a variable
-
 print(type(greeting))
 print(type(age))
 
