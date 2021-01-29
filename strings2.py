8d9ec20f2cd4d959cfd31f1c04a64bc4d668f8d1
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-01-28 19:09:24 -0500

    negative indexing with strings

--- strings2.py
+++ strings2.py
@@ -9,4 +9,12 @@ print(parrot[3])
 print(parrot[6])
 print(parrot[8])
 
+print()
+
+print(parrot[-11])
+print(parrot[-1])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
 
